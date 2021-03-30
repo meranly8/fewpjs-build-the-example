d6762fdefcbbc791b94594a840ab0610f38027d5
Michelle Ranly <meranly8@gmail.com>
2021-03-29 23:23:03 -0500

    commit tree e85397ec21caeff9e471c7a6dd80f7dd3b95a367

--- main.js
+++ main.js
@@ -3,9 +3,36 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.querySelector("#modal")
+modal.className = "hidden"
 
+const hearts = document.querySelectorAll(".like-glyph")
+hearts.forEach(heart => {
+  heart.addEventListener("click", likeArticle)
+})
 
+function likeArticle(event) {
+  const heart = event.target
 
+  mimicServerCall()
+  .then(() => {
+    if (heart.innerHTML === EMPTY_HEART) {
+      heart.innerHTML = FULL_HEART
+      heart.className = "activated-heart"
+    } else {
+      heart.innerHTML = EMPTY_HEART
+      heart.className = "like-glyph"
+    }
+  })
+  .catch(error => {
+    modal.className = ""
+    const modalMessage = document.querySelector("#modal-message")
+    modalMessage.innerHTML = error
+    setTimeout(() => {
+      modal.className = "hidden"
+    }, 4000)
+  })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
